1b394da18e135d441d5b3d6a2be03525e0fbc795
Olga Sergeeva <belka-ld@bk.ru>
2022-08-24 15:52:51 +0300

    add cards, add big cards, five sprint

--- scripts/index.js
+++ scripts/index.js
@@ -1,76 +1,3 @@
-//change popup profile
-
-const popup = document.querySelector('.popup')
-const formElement = document.querySelector('.registr')
-const userFirstName = document.querySelector('#firstName')
-const userLastName = document.querySelector('#lastName')
-const editButton = document.querySelector('.header__menu-edit')
-const closeButton = document.querySelector('.form-container__close')
-const userName = document.querySelector('.header__menu-user-name')
-const greetingUser = document.querySelector('.header__menu-greeting')
-const popupEditProfile = document.querySelector('.popup_edit_profile')
-
-//change popup-add
-
-
-const addButton = document.querySelector('.user-information__submit')
-const thoughtsPhoto = document.querySelector('#thoughtsPhoto')
-const thoughtsDescription = document.querySelector('#thoughtsDescription')
-const closeThoughtsForm = document.querySelector('#closeThoughtsForm')
-const popupAddCard = document.querySelector('.popup_add_card')
-const registrButtonAddSubmit = document.querySelector('.registr__submit')
-
-
-//change popup
-
-function openPopupProfileEditor() {
-    popupEditProfile.classList.add('popup_active')
-}
-
-function closePopup() {
-    popupEditProfile.classList.remove('popup_active')
-}
-
-function saveUserName() {
-    event.preventDefault()
-    userName.textContent = greetingUser.textContent + ', ' + userFirstName.value;
-    closePopup();
-}
-
-
-//change popupAdd
-
-
-function openPopupAdd() {
-    popupAddCard.classList.add('popup_active')
-}
-
-function closePopupAdd() {
-    popupAddCard.classList.remove('popup_active')
-}
-
-
-function getLikesActive(likeButton) {
-    likeButton.forEach(items, function () {
-
-    })
-}
-
-
-
-editButton.addEventListener('click', openPopupProfileEditor);
-closeButton.addEventListener('click', closePopup);
-formElement.addEventListener('submit', saveUserName);
-
-
-
-addButton.addEventListener('click', openPopupAdd);
-closeThoughtsForm.addEventListener('click', closePopupAdd);
-
-const userThoughtsDescription = document.querySelector('.user-thoughts__descriptio');
-const userThoughtsPic = document.querySelector('.user-thoughts__pic')
-
-
 //images array
 
 const userImagesArray = [
@@ -101,65 +28,155 @@ const userImagesArray = [
 ];
 
 
-const userLoadeds = document.querySelector('.user-loadeds');
+const userLoaded = document.querySelector('.user-loaded');
 
-for (let i = 0; i < userImagesArray.length; i++) {
+//All popups
 
-    const userLoadedTemplate = document.querySelector('#user-loaded').content.cloneNode(true);
-    
-    const userLoaded = userLoadedTemplate.querySelector('.message-photo');
-    
-    const messagePhoto = userLoadedTemplate.querySelector('.message-photo');
-    
-    userLoadedTemplate.querySelector('.message-photo__description').textContent = userImagesArray[i].name;
-    
-     userLoadedTemplate.querySelector('.message-photo__picture').src = userImagesArray[i].link;
-    
-    const likeButton = userLoadedTemplate.querySelector('.message-photo__like') 
-     likeButton.textContent = 'like';
-    
-     const trashButton = userLoadedTemplate.querySelector('.message-photo__trash');
+const popupEditProfile = document.querySelector('.popup_edit_profile');
+const popupAddCard = document.querySelector('.popup_add_card');
+const cardOpen = document.querySelector('.popup_card_open');
 
-    userLoadeds.append(userLoaded)
+//buttons
 
+const buttonCardEdit = document.querySelector('.header__menu-edit');
+const buttonAddCard = document.querySelector('.user-information__submit');
+const buttonClosePopupEditProfile = document.querySelector('.form-container__close');
+const buttonCloseThoughtsForm = document.querySelector('#closeThoughtsForm');
+const buttonCardClose = document.querySelector('#card-close');
+
+
+//forms, inputs
+
+const formEdd = document.querySelector('#form-edd');
+const formElement = document.querySelector('.registr')
+
+//form add card inputs
+
+const thoughtsPhoto = document.querySelector('#thoughtsPhoto');
+const thoughtsDescription = document.querySelector('#thoughtsDescription');
+
+//zoom inputs
+
+const cardOpenDescription = document.querySelector('.zoom-container__card-description');
+const cardOpenPic = document.querySelector('.zoom-container__card-pic');
+
+//profile edit inputs
+
+const userFirstName = document.querySelector('#firstName');
+const userLastName = document.querySelector('#lastName');
+
+//
+//profile variables
+const userName = document.querySelector('.header__menu-user-name');
+const greetingUser = document.querySelector('.header__menu-greeting');
+
+
+const templateCard = document.querySelector('#user-loaded-card').content
+
+
+function createCard(photo, titles) {
+
+    const messagePhoto = templateCard.querySelector('.message-photo').cloneNode(true);
+
+    const messagePhotoPicture = messagePhoto.querySelector('.message-photo__picture');
+
+    const messagePhotoDescription = messagePhoto.querySelector('.message-photo__description');
+
+
+    const likeButton = messagePhoto.querySelector('.message-photo__like')
+    likeButton.textContent = 'like';
+    likeButton.addEventListener('click', toggleLikeButton)
+
+
+    const trashButton = messagePhoto.querySelector('.message-photo__trash');
+
+
+    trashButton.addEventListener('click', function () {
+        const articleItem = trashButton.closest('.message-photo');
+        articleItem.remove();
+    })
+
+
+    messagePhotoPicture.src = photo;
+    messagePhotoPicture.alt = titles;
+    messagePhotoDescription.textContent = titles;
+
+
+    messagePhotoPicture.addEventListener('click', function () {
+        zoomImageCard(photo, titles)
+    })
+
+    //    userLoaded.prepend(messagePhoto)
+    return messagePhoto
 }
 
-//function addThoughtsPictureDescription () {
-//    evt.preventDefault;
-//    userImagesArray.name = userThoughtsDescription.value;
-//    userImagesArray.link = userThoughtsPic.src;   userImagesArray.append( userImagesArray.name, userImagesArray.link);
-//      closePopup();
-//}
 
-//registrButtonAddSubmit.addEventListener('click', function() {
-//    
-//});
+function renderCard(photo, titles) {
+    userLoaded.prepend(createCard(photo, titles))
+}
 
-//likes
-
-const likeButtons = document.querySelectorAll('.message-photo__like')
-
-
-
-likeButtons.forEach(function (items) {
-    items.addEventListener('click', function () {
-        items.classList.toggle('mesage-photo__like_active')
-    })
+userImagesArray.forEach(function (items) {
+    renderCard(items.link, items.name)
 
 })
 
+function handleAddCard() {
+    event.preventDefault()
+    renderCard(thoughtsPhoto.value, thoughtsDescription.value)
+    closePopup(popupAddCard)
+    formEdd.reset();
+}
 
-//delete cards
+function zoomImageCard(photo, titles) {
+    openPopup(cardOpen)
+    cardOpenPic.src = photo;
+    cardOpenDescription.textContent = titles;
+}
 
-const deleteCardsButtons = document.querySelectorAll('.message-photo__trash');
+function saveUserName() {
+    event.preventDefault()
+    userName.textContent = greetingUser.textContent + ', ' + userFirstName.value;
+    closePopup(popupEditProfile);
+}
+
+function toggleLikeButton(evt) {
+    evt.target.classList.toggle('mesage-photo__like_active')
+}
+
+function openPopup(popup) {
+    popup.classList.add('popup_active')
+}
+
+function closePopup(popup) {
+    popup.classList.remove('popup_active')
+}
 
 
-deleteCardsButtons.forEach(function (item) {
-        item.addEventListener('click', function () {
-            const articleItem = item.closest('.message-photo');
-            articleItem.remove();
-        })
-    }
 
+//listeners
 
-)
+formEdd.addEventListener('submit', handleAddCard);
+
+buttonCardEdit.addEventListener('click', function () {
+    openPopup(popupEditProfile)
+
+});
+
+buttonClosePopupEditProfile.addEventListener('click', function () {
+    closePopup(popupEditProfile)
+});
+
+formElement.addEventListener('submit', saveUserName);
+
+buttonAddCard.addEventListener('click', function () {
+    openPopup(popupAddCard)
+
+});;
+
+buttonCloseThoughtsForm.addEventListener('click', function () {
+    closePopup(popupAddCard)
+})
+
+buttonCardClose.addEventListener('click', function () {
+    closePopup(cardOpen)
+})
